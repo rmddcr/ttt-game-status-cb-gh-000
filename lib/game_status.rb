3023cae2958dffd1e47057c0cf9bcd2ff52af06a
Damitha Ranasingha <rmddcr@gmail.com>
2018-04-02 19:50:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,8 +3,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
-
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5],  # Middle row
@@ -17,19 +15,86 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+      if (board[combination[0]]=="O" && board[combination[1]]=="O" && board[combination[2]]=="O") ||
+         (board[combination[0]]=="X" && board[combination[1]]=="X" && board[combination[2]]=="X")
+          return true
+          break
+      end
+      
+    end
+    return false
+    
+  end
 
-  if  board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    return false
-  elsif full?(board)
-    return false
+
+
+
+# def won?(board)
+# 
+#   if  board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#     return false
+#   elsif full?(board)
+#     return false
+#   else
+#     i=0
+#     while i<9
+#       puts i
+#       combination=WIN_COMBINATIONS[i]
+#         if (board[combination[0]]=="X" && board[combination[1]]=="X" && board[combination[2]]=="X")
+# 
+#             return combination
+# 
+#         elsif (board[combination[0]]=="O" && board[combination[1]]=="O" && board[combination[2]]=="O")
+# 
+#           return combination
+#         else
+# 
+#         end
+#         i=i+1
+# 
+#       end
+#         return false
+# 
+#       end
+# 
+# end
+
+
+def full?(board)
+  index=0
+  while index<9
+    if !position_taken?(board,index)
+      return false
+      break
+    end
+    index +=1
+  end
+    return true
+end
+
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
   else
+    return false
+  end
+end
+
+
+
+
+
+def draw?(board)
+  if full?(board) 
     i=0
     while i<9
       puts i
       combination=WIN_COMBINATIONS[i]
         if (board[combination[0]]=="X" && board[combination[1]]=="X" && board[combination[2]]=="X")
 
-            return combination
+            return 
 
         elsif (board[combination[0]]=="O" && board[combination[1]]=="O" && board[combination[2]]=="O")
 
@@ -42,17 +107,5 @@ def won?(board)
       end
         return false
 
-      end
-
-end
-
-def full?(board)
-  index=0
-  while index <9
-    if board[index]==" "
-      return false
-    end
-    index +=1
   end
-    return true
 end
