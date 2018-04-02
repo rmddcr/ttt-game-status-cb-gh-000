6924ebb5d5c2feeb69eb869c5b3be2e77b47600b
Damitha Ranasingha <rmddcr@gmail.com>
2018-04-02 18:52:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,17 +23,17 @@ def won?(board)
 
   i=i+1
     if (board[combination[0]]=="X" && board[combination[1]]=="X" && board[combination[2]]=="X")
-      
+
       return combination
 
     elsif (board[combination[0]]=="O" && board[combination[1]]=="O" && board[combination[2]]=="O")
-        
+
         return combination
     else
-      
+
     end
-    
-    
+
+
 
   end
   return false
