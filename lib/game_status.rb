51fb7279a9694ae0c7048cdd9bc7869b1215f187
Damitha Ranasingha <rmddcr@gmail.com>
2018-04-02 18:02:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ def won?(board)
   WIN_COMBINATIONS.each? { |combination|
     if  (board[combination[0]]=="X" && board[combination[1]]=="X" && board[combination[2]]=="X") ||
         (board[combination[0]]=="O" && board[combination[1]]=="O" && board[combination[2]]=="O")
-        
+
         return combination
     else
 
