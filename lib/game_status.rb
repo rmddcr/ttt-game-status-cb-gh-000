e1ae36a12ae08919981d99a38ddc244dadcdfe8f
Damitha Ranasingha <rmddcr@gmail.com>
2018-04-02 18:21:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,21 +19,17 @@ WIN_COMBINATIONS = [
 def won?(board)
 
 
-  # WIN_COMBINATIONS.each? do |combination|
-  #   if  (board[combination[0]]=="X" && board[combination[1]]=="X" && board[combination[2]]=="X") ||
-  #       (board[combination[0]]=="O" && board[combination[1]]=="O" && board[combination[2]]=="O")
-  #
-  #       return combination
-  #   else
-  #
-  #     return false
-  #   end
-  #
-  # end
-
-end
-  }
-
+  WIN_COMBINATIONS.each? do |combination|
+    if  (board[combination[0]]=="X" && board[combination[1]]=="X" && board[combination[2]]=="X") ||
+        (board[combination[0]]=="O" && board[combination[1]]=="O" && board[combination[2]]=="O")
+  
+        return combination
+    else
+  
+      return false
+    end
+  
+  end
 
 end
 
